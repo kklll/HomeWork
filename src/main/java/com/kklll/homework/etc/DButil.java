0d126a8896a0eee58ca5757cd90acb0e23985673
kklll <the__deep@163.com>
2020-05-06 21:37:31 +0800

    第一次提交

--- src/main/java/com/kklll/homework/etc/DButil.java
+++ src/main/java/com/kklll/homework/etc/DButil.java
@@ -8,9 +8,9 @@ import java.sql.DriverManager;
  * @date 2020/3/17 19:15
  */
 public class DButil {
-    private static String url="jdbc:mysql://db.dlddw.xyz:10083/DB?characterEncoding=UTF-8&&serverTimezone=UTC";
+    private static String url="jdbc:mysql://xxxxx:10083/DB?characterEncoding=UTF-8&&serverTimezone=UTC";
     private static String username="root";
-    private static String password="hades54524";
+    private static String password="xxxxxx";
     public static Connection getConnect() throws Exception {
         //是否有数据库驱动
         Class.forName("com.mysql.cj.jdbc.Driver");
